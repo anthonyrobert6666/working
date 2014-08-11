d6ba8902f1115e526c1041d04fd8c9aba81bb779
anthonyrobert6666 <anthonyrobert6666@gmail.com>
2014-08-11 17:33:19 +0700

    create project

---
+++ PenStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PenStore.Startup))]
+namespace PenStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
