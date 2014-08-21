58999a3bde8b8df87825c9fa4bc5bc76eb45489e
anthonyrobert6666 <anthonyrobert6666@gmail.com>
2014-08-21 12:35:39 +0700

    1.13
    change password: ok

---
+++ PenParadise_1.12/PenParadise/Models/OrderDetail_Validate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+namespace PenParadise.Models
+{
+    [MetadataType(typeof(OrderDetail_Validation))]
+    public partial class OrderDetail
+    {
+
+    }
+    public class OrderDetail_Validation
+    {
+        [Required]
+        [Display(Name = "ID")]
+        public string OrderDetailID { get; set; }
+        [Required]
+        [Display(Name = "Order")]
+        public string OrderID { get; set; }
+        [Required]
+        [Display(Name = "Product Name")]
+        public string ProductID { get; set; }
+        [Required]
+        public Nullable<int> Quantity { get; set; }
+        [Required]
+        [Display(Name = "Price")]
+        public Nullable<double> UnitPrice { get; set; }
+        [Required]
+        public virtual Order Order { get; set; }
+        [Required]
+        public virtual Product Product { get; set; }
+    }
+
+}
---
+++ PenParadise_1.12/PenParadise/Models/Order_Validate.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+namespace PenParadise.Models
+{
+    [MetadataType(typeof(Order_Validation))]
+    public partial class Order
+    {
+
+    }
+    public class Order_Validation
+    {
+        [Required]
+        [Display(Name = "ID")]
+        public string OrderID { get; set; }
+
+        [Required]
+        [Display(Name = "User Name")]
+        public string UserNameID { get; set; }
+
+        [Required]
+        [Display(Name = "Order Date")]
+        [DataType(DataType.DateTime)]
+        public string OrderDate { get; set; }
+
+        [Required]
+        [Display(Name = "Delivery Address")]
+        public string DeliveryAddress { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        [Display(Name = "Phone Contact")]
+        [RegularExpression("^[0-9]{10}$", ErrorMessage = "Invalid Phone Number!")]
+        public string PhoneContact { get; set; }
+
+        [Required]
+        public Nullable<int> Total { get; set; }
+
+        [Required]
+        public virtual User User { get; set; }
+    }
+
+}
